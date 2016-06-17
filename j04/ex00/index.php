8fd3ad9539553d7cce83cfc563f0c999eebb84f6
Jordan MUNOZ <jmunoz@e1r10p19.42.fr>
2016-06-17 20:17:56 +0200

    j04

---
+++ j04/ex00/index.php
@@ -0,0 +1,31 @@
+<?PHP
+session_start();
+if (isset($_COOKIE['PHPSESSID']))
+{
+	if (!strcmp($_GET['submit'], "OK"))
+	{
+		$_SESSION['login'] = $_GET['login'];
+		$_SESSION['passwd'] = $_GET['passwd'];
+	}
+}
+?>
+<html>
+<body>
+<form ...>
+Identifiant: <input type="text" name="login" value="
+<?PHP 
+if (isset($_SESSION['login']))
+	echo $_SESSION['login']; 
+?>
+" />
+<br />
+Mot de passe: <input type="password" name="passwd" value="
+<?PHP 
+if (isset($_SESSION['passwd']))
+	echo $_SESSION['passwd']; 
+?>
+"/>
+<br />
+<a href='index.php'><input type="submit" name="submit" value="OK" /></a>
+</form>
+</body></html>
