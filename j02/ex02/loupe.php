288d3903c62ec6e467eccc8bf35678a12343e2e9
Martingale <j1805@hotmail.com>
2016-12-31 00:41:14 +0100

    j02/ex02 é

--- j02/ex02/loupe.php
+++ j02/ex02/loupe.php
@@ -1,14 +1,14 @@
 #!/usr/bin/php
 <?PHP
 function h($array) {
-	$title = preg_replace_callback("/(title=\")(.*?)(\")/si", j,  $array[0]);
-	$title = preg_replace_callback("/(>)(.*?)(<)/si", j, $title);
+	$title = preg_replace_callback("/(title=\")([^\"]*)(\")/si", j,  $array[0]);
+	$title = preg_replace_callback("/(>)([^<]+)(<)/si", j, $title);
 	return $title;
 }
 function j($array) {
 	return $array[1].strtoupper($array[2]).$array[3];
 }
 $page = file_get_contents($argv[1]);
-$page = preg_replace_callback("/<a\s.+?>.+?<\/a>/is", h,  $page);
+$page = preg_replace_callback("/<a(\s+[^>]*)*>.+?<\/a>/is", h,  $page);
 echo "$page";
 ?>
