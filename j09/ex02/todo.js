521e223d27028e0248c9540703c5c377330e756d
Martingale <j1805@hotmail.com>
2017-01-20 03:53:18 +0100

    p

--- j09/ex02/todo.js
+++ j09/ex02/todo.js
@@ -1,60 +1,98 @@
+var cookie_lifespan = 10;
+var d = new Date;
+d.setTime(d.getTime() + (cookie_lifespan * 86400000));
+var expires = "; expires="+d.toString();
+console.log("BEGIN"+expires);
+var kill = "; expires= Mon, 18 Dec 1995 17:28:35 GMT";
+var ft_list = document.getElementById("ft_list");
+var cookie_string = document.cookie;
+var cookie_nb;
 
+//delete_cookie();
+recover_todo();
 
-function	get_cookie_nb() {
-	if (document.cookie) {
-		var matchl
-		if ((match = document.cookie.match("/cookie_nb=*;/"))) {
-			console.log(match[0]);
-			return (parseInt(match[0]));
+function	recover_todo() {
+	ar = {};
+	console.log(cookie_string);
+	if (cookie_string.length > 0) {
+		console.log("OJDSD");
+		console.log(cookie_string);
+		var c = cookie_string.split(';');
+		for (var i = 0; i < c.length; i++) {
+			var	d = c[i].split('=');
+			if (d[1] && d[0]) {
+				ar[d[0].trim()] = d[1];
+			}
 		}
-		else {
-		document.cookie = "cookie_nb=0 ; expires=Mon, 18 Dec 1995 17:28:35 GMT";
+		cookie_nb = ar.count;
+		for (key in ar) {
+			if (key == "cookie_nb") {
+				cookie_nb = ar[key];
+			}
+			else {
+				var todo = create_todo(key, ar[key]);
+				insert_todo(todo);
+			}
 		}
-	}
-	else
-		return (0);
-}
-
-function	delete_cookie(todo_id) {
-
-	document.cookie = todo_id+"= ; expires=Mon, 18 Dec 1995 17:28:35 GMT";
-	document.cookie = "cookie_nb="+(get_cookie_nb()--)
-}
-
-function	add_cookie(todo_id, todo_str) {
-	console.log(todo_id);
-	console.log(todo_str);
-	todo_str.replace("/;/", "%%");
-	var n_date = new Date();
-	n_date.setTime(n_date.getTime() + (7*86400000));
-	var expires = "; expires="+n_date.toUTCString();
-	document.cookie = todo_id+"="+todo_str+expires;
-	document.cookie = "cookie_nb="+(get_cookie_nb()++)
-}
-
-function	insert_todo() {
-	var ft_list = document.getElementById("ft_list");
-	var todo = document.createElement("div");
-	var text = prompt("Please indicate what you have todo!");
-	todo.innerHTML = text;
-	todo.className = "todo";
-	todo.id = "todo_"+get_cookie_nb();
-	todo.onclick = function() {
-		if (confirm("Etes-vous sûr ?") == true) {
-			this.parentNode.removeChild(this)
-			delete_cookie(this.id);
-		}
-	}
-	if (ft_list.childElementCount == 0) {
-		ft_list.appendChild(todo);
-	   add_cookie(todo.id, todo.innerHTML);
 	}
 	else {
-		ft_list.insertBefore(todo, ft_list.firstChild);
-		add_cookie(todo.innerHTML);
+		cookie_nb = 0;
+		}
+}
+
+function	create_todo(id, content) {
+		console.log(content);
+		content = document.createTextNode(content);
+		var todo = document.createElement("div");
+		todo.className = "todo";
+		todo.id = id;
+		todo.appendChild(content);
+		todo.onclick = function() {
+			if (confirm("Etes-vous sûr ?") == true) {
+				this.parentNode.removeChild(this);
+				document.cookie = id+"="+content+kill;
+				cookie_nb--;
+				console.log(cookie_nb);
+				document.cookie = "cookie_nb="+cookie_nb;
+			}
+		};
+		return (todo);
+}
+
+function	insert_todo(todo) {
+		cookie_string = document.cookie;
+		if (ft_list.childElementCount === 0) {
+			ft_list.appendChild(todo);
+			document.cookie = todo.id+"="+todo.textContent+expires;
+			console.log("COOK"+cookie_string);
+		}
+		else {
+			ft_list.insertBefore(todo, ft_list.firstChild);
+			document.cookie = todo.id+"="+todo.textContent+expires;
+			console.log("COOK"+cookie_string);
+		}
+
+}
+			
+
+function f() {
+	var content = prompt("What to do ?")
+	if (content === null) {
+		return ;
+	}
+	content = content.trim();
+	if (content.length > 0){
+		console.log(cookie_nb);
+		var todo = create_todo(cookie_nb, content);
+		insert_todo(todo);
+		cookie_nb++;
+		document.cookie = "cookie_nb="+cookie_nb;
 	}
 }
 
-
-
-
+function delete_cookie() {
+	var res = document.cookie.split(';');
+	res.forEach(function (x) {
+		console.log(x+kill);
+		document.cookie =x+kill;});
+}
