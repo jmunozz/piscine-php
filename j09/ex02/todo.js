8e40ffe1bab90ea56f45387f53167712167cba28
Jordan Munoz <jordan@alveo.io>
2018-01-20 02:26:55 +0100

    clean ex02

--- j09/ex02/todo.js
+++ j09/ex02/todo.js
@@ -1,101 +1,80 @@
-var cookie_lifespan = 10;
-var d = new Date;
-d.setTime(d.getTime() + (cookie_lifespan * 86400000));
-var expires = "; expires="+d.toString();
-console.log("BEGIN"+expires);
-var kill = "; expires= Mon, 18 Dec 1995 17:28:35 GMT";
-var ft_list = document.getElementById("ft_list");
-var cookie_string = document.cookie;
-var cookie_nb;
 
-//delete_cookie();
+let date = new Date();
+date = date.setTime(date.getTime() + 864000000);
+let EXPIRES = `expires=${date.toString()}`;
+let KILL = `expires= Mon, 18 Dec 1995 17:28:35 GMT`;
+let cookie_nb = 0;
+const ft_list = document.getElementById("ft_list");
+
 recover_todo();
 
-function	recover_todo() {
-	ar = {};
-	console.log(cookie_string);
-	if (cookie_string.length > 0) {
-		console.log("OJDSD");
-		console.log(cookie_string);
-		var c = cookie_string.split(';');
-		for (var i = 0; i < c.length; i++) {
-			var	d = c[i].split('=');
-			if (d[1] && d[0]) {
-				ar[d[0].trim()] = d[1];
-			}
-		}
-		cookie_nb = ar.count;
-		for (key in ar) {
-			if (key == "cookie_nb") {
-				cookie_nb = ar[key];
-			}
-			else {
-				var todo = create_todo(key, ar[key]);
-				insert_todo(todo);
-			}
-		}
+
+// Get All Cookies and transform them into todo list.
+function recover_todo() {
+
+	const allCookiesArray = document.cookie.split('; ').filter((cookie) => {
+			return cookie.substring(0, 5) == 'todo_'});
+	if (allCookiesArray.length > 0) {
+		let last_cookie_id; 
+		allCookiesArray.forEach(cookie => {
+			const c = cookie.split('=')
+			cookie_id = c[0];
+			cookie_content = c[1];
+			console.log(cookie_id, cookie_content);
+			let todo = create_todo(cookie_id, cookie_content);
+			insert_todo(todo);
+			last_cookie_id = cookie_id;
+		})
+		cookie_nb = parseInt(last_cookie_id.substring(5)) + 1;
+		console.log(cookie_nb);
 	}
-	else {
-		cookie_nb = 0;
-		}
 }
 
-function	create_todo(id, content) {
-		console.log("CREATE_CONTENT= "+content);
-		console.log("CREATE_ID= "+id);
-		content = document.createTextNode(content);
-		var todo = document.createElement("div");
+function create_todo(id, content) {
+
+
+		// Create all elements
+		let todo = document.createElement("div");
+		let contentNode = document.createTextNode(content);
+
+		// Add all elements to DOM element.
 		todo.className = "todo";
 		todo.id = id;
-		todo.appendChild(content);
+		todo.appendChild(contentNode);
 		todo.onclick = function() {
-			if (confirm("Etes-vous sûr ?") == true) {
+			if (confirm("Etes-vous sur ?") == true) {
 				k = this.id;
 				this.parentNode.removeChild(this);
-				document.cookie = k+"=''"+kill;
-				cookie_nb--;
-				console.log("DELETE_COOKIE_NB= "+cookie_nb);
-				document.cookie = "cookie_nb="+cookie_nb;
-				console.log("DELETE_COOKIE_STRING= "+document.cookie);
+				deleteOneCookie(id);
 			}
 		};
 		return (todo);
 }
 
-function	insert_todo(todo) {
-		if (ft_list.childElementCount === 0) {
-			ft_list.appendChild(todo);
-			document.cookie = todo.id+"="+todo.textContent+expires;
-			console.log("INSERT_COOKIE_STRING= "+document.cookie);
-		}
-		else {
-			ft_list.insertBefore(todo, ft_list.firstChild);
-			document.cookie = todo.id+"="+todo.textContent+expires;
-			console.log("INSERT_COOKIE_STRING= "+document.cookie);
-		}
-
+function insert_todo(todo) {
+		if (ft_list.childElementCount === 0) ft_list.appendChild(todo);
+		else ft_list.insertBefore(todo, ft_list.firstChild);
 }
 			
 
 function f() {
-	var content = prompt("What to do ?")
-	if (content === null) {
-		return ;
-	}
-	content = content.trim();
-	if (content.length > 0){
-		console.log("f_COOKIE_NB = "+cookie_nb);
-		var todo = create_todo(cookie_nb, content);
+
+	//Get todo content
+	let content = prompt("What to do ?")
+	if(content && content.trim() !== "") {
+		let id = `todo_${cookie_nb++}`;
+		//Create todo element
+		let todo = create_todo(id, content.trim());
+		//Insert todo element
 		insert_todo(todo);
-		cookie_nb++;
-		document.cookie = "cookie_nb="+cookie_nb;
-		console.log("F__COOKIE_STRING= "+document.cookie);
+		insertOneCookie(id, content.trim());
 	}
 }
 
-function delete_cookie() {
-	var res = document.cookie.split(';');
-	res.forEach(function (x) {
-		console.log(x+kill);
-		document.cookie =x+kill;});
+function deleteOneCookie(id) {
+	document.cookie = `${id}=delete; ${KILL}`
+}
+
+function insertOneCookie(id, content) {
+	document.cookie = `${id}=${content}; ${EXPIRES}`;
 }
